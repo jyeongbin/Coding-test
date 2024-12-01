827e104a56cebbdb4945b27dada72bfbcf389484
배정빈 <wjdqls0530@naver.com>
2024-12-01 14:45:51 +0900

    A/B

---
+++ 80933629.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a, b;
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+
+        System.out.println(a/b);
+    }
+}
